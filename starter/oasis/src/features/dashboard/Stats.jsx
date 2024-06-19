f03173d439bfd5de111e332c85682d5b8c0344b4
Baejita <homokoro@gmail.com>
2024-06-19 17:51:24 +0700

    stats

---
+++ starter/oasis/src/features/dashboard/Stats.jsx
@@ -0,0 +1,53 @@
+import { formatCurrency } from "../../utils/helpers";
+import Stat from "./Stat";
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+// eslint-disable-next-line react/prop-types
+function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
+  //1.
+  const numBookings = bookings.length;
+
+  //2.
+  const sales = bookings.reduce((acc, cur) => acc + cur.totalPrice, 0);
+
+  const checkIns = confirmedStays.length;
+
+  const occupation =
+    confirmedStays.reduce((acc, cur) => acc + cur.numNights, 0) /
+    (numDays * cabinCount);
+
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales)}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkIns}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={Math.round(occupation * 100) + "%"}
+      />
+    </>
+  );
+}
+
+export default Stats;
--- starter/oasis/src/features/dashboard/useRecentStays.js
+++ starter/oasis/src/features/dashboard/useRecentStays.js
@@ -19,5 +19,5 @@ export function useRecentStays() {
   const confirmedStays = stays?.filter(
     (stay) => stay.status === "checked-in" || stay.status === "checked-out"
   );
-  return { isLoading, confirmedStays, stays };
+  return { isLoading, confirmedStays, stays, numDays };
 }
